f7474881c024b9f420bc799535aee24655420c78
hilmimuharromi <hilmimuharrom@gmail.com>
2021-09-16 11:07:38 +0700

    add setting procfile

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const {register} = require('../controllers/userController')
 const productRouter = require('./productsRouter')
+router.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'server is running'
+    })
+})
 router.use('/products', productRouter)
 router.post('/register', register)
 
